d712ff4f152a89b0cd56891df786d2fa4714b334
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-10-12 06:47:06 +0530

    added the initial files and Login Signup Page

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import { Route, Routes } from "react-router-dom";
+import Signinform from "./_Auth/Form/Signinform";
+import Signupform from "./_Auth/Form/Signupform";
+import AuthLayout from "./_Auth/AuthLayout";
+import RootLayout from "./Root/RootLayout";
+import './index.css'
+import { Home } from "./Root/Pages";
+
+function App() {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<Signinform />} />
+          <Route path="/sign-up" element={<Signupform />} />
+        </Route>
+ 
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Root/RootLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <>
+      <header>
+        {/* You can add a navbar or header here */}
+        <h1>My App Header</h1>
+      </header>
+
+      
+    </>
+  );
+}
