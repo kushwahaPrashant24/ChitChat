2497d517b25ab1ed7a200fac7e1b6b0148ffdbbb
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-11-21 02:02:04 +0530

    added the chat section

--- client/src/pages/chat/Components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/Components/chat-container/components/message-bar/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import {GrAttachment } from "react-icons/gr"
 
 function MessageBar() {
   return (
-    <div>MessageBar</div>
-  )
+    <div className="h-[10vw] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6 ">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5]">
+        <input
+          type="text"
+          className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none "
+          placeholder="Enter Message"
+       
+        />
+        <button className=' text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all '>
+          <GrAttachment  className="text-2xl " />
+        </button>
+        
+      </div>
+    </div>
+  );
 }
 
-export default MessageBar
+export default MessageBar;
--- client/src/pages/chat/Components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/Components/chat-container/components/message-container/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function MessageContainer() {
-  return <div>MessageContainer</div>;
+  return <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:[75vw] xl:[80vw] w-full ">
+  
+  </div>;
 }
 
 export default MessageContainer;
