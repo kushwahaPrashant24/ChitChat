2558fc812f572398fa1387d0cc5adb7bff793edc
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-12-29 16:26:04 +0530

    completed the comtact field

--- client/src/components/contact-list.jsx
+++ client/src/components/contact-list.jsx
@@ -1,6 +1,8 @@
 import { userAppStore } from "@/Store";
 import React from "react";
 import { HOST } from "@/utils/constants";
+import { getColor } from "@/lib/utils";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 export default function ContactList({ contacts, isChannel = false }) {
   const {
@@ -27,12 +29,12 @@ export default function ContactList({ contacts, isChannel = false }) {
 
   return (
     <div className="mt-5">
-     { /*{contacts.map((contact) => (
+      {contacts.map((contact) => (
         <div
           key={contact._id}
           className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
             selectedChatData && selectedChatData._id === contact._id
-              ? "bg-[#8417ff] hover:bg-[#8417ff]"
+              ? "bg-[#7829f6] hover:bg-[#8417ff]"
               : "hover:bg-[#f1f1f111]"
           }`}
           onClick={() => handleClick(contact)}
@@ -40,28 +42,40 @@ export default function ContactList({ contacts, isChannel = false }) {
           <div className="flex gap-5 items-center justify-start text-neutral-300">
             {!isChannel && (
               <Avatar className="h-10 w-10 rounded-full overflow-hidden">
-                {selectedChatData.image ? (
+                {contact.image ? (
                   <AvatarImage
-                    src={`${HOST}/${selectedChatData.image}`}
+                    src={`${HOST}/${contact.image}`}
                     alt="profile"
                     className="object-cover w-full h-full bg-black"
                   />
                 ) : (
                   <div
-                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
-                      selectedChatData.color
+                    className={`
+                    ${selectedChatData && selectedChatData._id === contact._id ? "bg-[ffffff22]  border border-white/70 " : `${getColor(
+                      contact.color
                     )}`}
+                    uppercase h-10 w-10 text-lg border-[1px] flex items-center justify-center rounded-full`}
                   >
-                    {selectedChatData.firstName
-                      ? selectedChatData.firstName.charAt(0)
-                      : selectedChatData.email.charAt(0)}
+                    {contact.firstName
+                      ? contact.firstName.charAt(0)
+                      : contact.email.charAt(0)}
                   </div>
                 )}
               </Avatar>
             )}
+            {isChannel && (
+              <div className="bg-[#ffffff22] h-10 w-10  flex items-center justify-center rounded-full">
+                #
+              </div>
+            )}
+            {isChannel ? (
+              <span>{contact.name}</span>
+            ) : (
+              <span>{`${contact.firstName} ${contact.lastName}`}</span>
+            )}
           </div>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
