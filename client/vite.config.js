6ce2692409d4252c2cbc625654b073bbca87375f
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2025-01-03 19:36:34 +0530

    added multiselect feature

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@mui/material', '@mui/icons-material'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
