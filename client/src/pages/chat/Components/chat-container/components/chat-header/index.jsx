330d9936daf11f143fa1789b0288a9151dc994c5
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2025-01-04 03:34:16 +0530

    added the channel ui

--- client/src/pages/chat/Components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/Components/chat-container/components/chat-header/index.jsx
@@ -13,31 +13,36 @@ function Chatheader() {
       <div className="flex gap-5 items-center justify-between w-full">
         <div className="flex gap-3 items-center justify-center  ">
           <div className="w-12 h-12 relative">
-            <Avatar className="h-12 w-12 rounded-full overflow-hidden">
-              {selectedChatData.image ? (
-                <AvatarImage
-                  src={`${HOST}/${selectedChatData.image}`}
-                  alt="profile"
-                  className="object-cover w-full h-full bg-black"
-                />
-              ) : (
-                <div
-                  className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
-                    selectedChatData.color
-                  )}`}
-                >
-                  {selectedChatData.firstName
-                    ? selectedChatData.firstName.charAt(0)
-                    : selectedChatData.email.charAt(0)}
-                </div>
-              )}
-            </Avatar>
+            {selectedChatType === "contact" ? (
+              <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                {selectedChatData.image ? (
+                  <AvatarImage
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="profile"
+                    className="object-cover w-full h-full bg-black"
+                  />
+                ) : (
+                  <div
+                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                      selectedChatData.color
+                    )}`}
+                  >
+                    {selectedChatData.firstName
+                      ? selectedChatData.firstName.charAt(0)
+                      : selectedChatData.email.charAt(0)}
+                  </div>
+                )}
+              </Avatar>
+            ) : (
+              <div className="bg-[#ffffff22] h-10 w-10 flex items-center justify-center rounded-full">
+                #
+              </div>
+            )}
           </div>
-          
-            {selectedChatType === "contact" &&selectedChatData.firstName
-              ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
-              : selectedChatData.email}
-          
+          {selectedChatType === "channel" && selectedChatData.name };
+          {selectedChatType === "contact" && selectedChatData.firstName
+            ? `${selectedChatData.firstName} ${selectedChatData.lastName}`
+            : selectedChatData.email}
         </div>
         <div className="flex items-center justify-center gap-5">
           <button
